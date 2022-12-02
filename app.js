6a3e0cc92e67e22dba6e230232bdd76a6b5971ab
Denis <hardware174@yandex.ru>
2022-12-03 00:18:58 +0500

    fix js price

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ if (age >2020)
 {
     console.log('Человек по имени: ' + name + ', сейчас имеет возраст: ' + age + ' лет')
 } else {
-    console.log('Этоо будущее')
+    console.log('hello this is github')
 }
 }
 
